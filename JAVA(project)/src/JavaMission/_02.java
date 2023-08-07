8fa5a570c102a5524721e73c97c25cc601fedc44
junghoonlee <junghoonlee@218.38.137.27>
2023-08-08 01:47:42 +0900

    집에서 만든 문

--- JAVA(project)/src/JavaMission/_02.java
+++ JAVA(project)/src/JavaMission/_02.java
@@ -9,26 +9,43 @@ public class _02 {
 		int N = sc.nextInt();
 		int sum1 = 0;
 		int sum2 = 0;
-		for (int i = 0; i < N; i++) {
-			if(i % 2 == 1 ) {
+		for (int i = 1; i <= N; i+=2) {
+			if(i < N-1 ) {
 				sum1 += i;
-				
+				System.out.print(i + "+");
 			}
 		}
+		if(N % 2 == 1){
+			System.out.print(N);
+			sum1 += N;
+		}
+		else {
+			System.out.print(N-1);
+			sum1 += N-1;
+		}
+		System.out.print("=" + sum1);
 		System.out.println();
-		for (int i = 0; i < N; i++) {
-			if(i % 2 == 0 ) {
+		for (int i = 2; i <= N; i += 2) {
+			if(i < N) {
 				sum2 += i;
+			System.out.print(i + "+");
 			}
 		}
+		if(N %2 == 0) {
+			System.out.print(N);
+			sum2 += N;
+		}
+		else {
+			System.out.print(N-1);
+			sum2 += N-1;
+		}
+		System.out.print("=" + sum2);
 		System.out.println();
-		for (int i = 0; i < N; i++) {
-			if(N % i == 1) {
+		for (int i = 1; i <= N; i++) {
+			if(N % i == 0) {
 				System.out.print(i + " ");
 			}
 		}
-		
-		
 		sc.close();
 	}
 }
